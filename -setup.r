bf650f37c2a847798dff2f0f738e1f4d9d60859b
njlyon0 <nicholasjlyon@gmail.com>
2026-05-11 15:08:34 -0400

    feat (setup): create several 'raw' folders for diff categories of data

--- -setup.r
+++ -setup.r
@@ -12,7 +12,9 @@ rm(list = ls()); gc()
 ## ----------------------------- ##
 
 # Create necessary folders
-dir.create(path = file.path("data", "raw"), showWarnings = FALSE, recursive = TRUE)
+dir.create(path = file.path("data", "raw-fire"), showWarnings = FALSE, recursive = TRUE)
+dir.create(path = file.path("data", "raw-climate"), showWarnings = FALSE)
+dir.create(path = file.path("data", "raw-veg"), showWarnings = FALSE)
 dir.create(path = file.path("data", "tidy"), showWarnings = FALSE)
 dir.create(path = file.path("graphs"), showWarnings = FALSE)
 
